db3a122c6dd362124da787b71efae24bc5b9825c
GiangGiang1105 <nnhgiang.17it1@vku.udn.vn>
2021-04-06 11:01:20 +0700

    create sqlite to save favourite meal

---
+++ app/src/main/java/com/example/gfood/data/source/local/database/table/MealFavouriteTable.kt
@@ -0,0 +1,8 @@
+package com.example.gfood.data.source.local.database.table
+
+object MealFavouriteTable {
+    const val TABLE_NAME = "Meal_Favourite"
+    const val COLUMN_NAME = "name"
+    const val COLUMN_ID_MEAL = "id_meal"
+    const val COLUMN_IMAGE = "image"
+}
---
+++ app/src/main/java/com/example/gfood/data/source/repository/MealFavouriteRepository.kt
@@ -0,0 +1,36 @@
+package com.example.gfood.data.source.repository
+
+import com.example.gfood.data.model.Meal
+import com.example.gfood.data.source.MealFavouriteDataSource
+import com.example.gfood.data.source.local.OnFetchDataLocalListener
+
+class MealFavouriteRepository(
+    private val local: MealFavouriteDataSource.Local
+) {
+    fun save(
+        entity: Meal,
+        listener: OnFetchDataLocalListener<Long>
+    ) {
+        local.save(entity, listener)
+    }
+
+    fun getListMealFavourite(
+        listener: OnFetchDataLocalListener<MutableList<Meal>>
+    ) {
+        local.getListMealFavourite(listener)
+    }
+
+    fun delete(
+        idEntity: String?,
+        listener: OnFetchDataLocalListener<Int>
+    ) {
+        local.delete(idEntity, listener)
+    }
+
+    fun getMeal(
+        idEntity: String?,
+        listener: OnFetchDataLocalListener<Boolean>
+    ) {
+        local.getMeal(idEntity, listener)
+    }
+}
